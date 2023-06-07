5832c7385fa085bfaeb4b688620a28ffc088fd8c
kacper <kacpersloka@o2.pl>
2023-06-07 10:56:20 +0200

    cool

--- index.js
+++ index.js
@@ -15,12 +15,18 @@ function render() {
         <a class="thumbnail" href="https://averageuser4.github.io/${data.name}">
           <img class="thumbnail__image" src="assets/${data.name}.jpg">
         </a>
-        <div class="description">
-          <h3>${data.presentationName}</h3>
-          <p>${data.technologies}</p>
-          <p>${data.description}</p>
-          <a target="_blank" href="https://github.com/AverageUser4/${data.name}">Source Code</a> |
-          <a target="_blank" href="https://averageuser4.github.io/${data.name}">Live on GitHub</a>
+        <div class="text-container">
+          <div>
+            <a target="_blank" href="https://averageuser4.github.io/${data.name}">
+              <h3>${data.presentationName}</h3>
+            </a>
+            <p class="technologies">${data.technologies}</p>
+            <p class="desc">${data.description}</p>
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/AverageUser4/${data.name}">Source Code</a> |
+            <a target="_blank" href="https://averageuser4.github.io/${data.name}">Live on GitHub</a>
+          </div>
         </div>
       </li>
     `;
--- projects.js
+++ projects.js
@@ -1,12 +1,18 @@
 export default [
   {
-    name: 'kalkulator',
-    presentationName: 'Windows Calculator Remake',
-    technologies: 'HTML, CSS, JavaScript',
-    description: `One of my first JavaScript projects (i had some prior programming experience).
-      It's an exact copy of Windows 10's calculator, supporting all calculations from
-      the "standard" section as well as memory and history features. It can be moved around,
-      resized and operated with keyboard or mouse.`,
+    name: 'paint',
+    presentationName: 'MS Paint Remake',
+    technologies: 'React, Canvas API',
+    description: `Supports most features of MS Paint (selection, shapes, flood fill, etc.). 
+    It can be moved around, resized and embedded on any page. There are still some minor
+    things I want to add / fix, but it's mostly finished.`,
+  },
+  {
+    name: 'salinka',
+    presentationName: 'Salinaka (E-commerce SPA)',
+    technologies: 'Typescript, React, Redux, React Router',
+    description: `Single Page Application based on project by JULIUS GUEVARRA. Stores user and cart
+    data inside Redux store. Was mostly built to get comfortable with Redux, so it's not very polished.`,
   },
   {
     name: 'kalkulator',
