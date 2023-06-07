d71d3f1cd48008702e91ad6a350a8fc5c92938be
kacper <kacpersloka@o2.pl>
2023-06-07 21:27:38 +0200

    changed names of project

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 export default [
   {
-    name: 'paint',
+    name: 'ms-paint-remake',
     presentationName: 'MS Paint Remake',
     technologies: 'React, Canvas API',
     description: `Supports most features of MS Paint (selection, shapes, flood fill, etc.). 
@@ -8,15 +8,15 @@ export default [
     things I want to add / fix, but it's mostly finished.`,
   },
   {
-    name: 'salinka',
-    presentationName: 'Salinaka (E-commerce SPA)',
+    name: 'e-commerce-salinaka',
+    presentationName: 'E-Commerce SPA - Salinaka',
     technologies: 'Typescript, React, Redux, React Router',
     description: `Single Page Application based on project by JULIUS GUEVARRA. Stores user and cart
     data inside Redux store. Was mostly built to get comfortable with Redux, so it's not very polished.`,
   },
   {
-    name: 'kalkulator',
-    presentationName: 'Windows Calculator Remake',
+    name: 'windows-10-calculator-remake',
+    presentationName: 'Windows 10 Calculator Remake',
     technologies: 'HTML, CSS, JavaScript',
     description: `One of my first JavaScript projects (i had some prior programming experience).
       It's an exact copy of Windows 10's calculator, supporting all calculations from
