6a8b7f970ed21b0f62cb9e95580e7636aafc29da
kacper <kacpersloka@o2.pl>
2023-06-10 13:53:10 +0200

    improved website design

--- index.js
+++ index.js
@@ -10,19 +10,27 @@ function render() {
     projectsList.innerHTML += `
       <li class="project">
         <a  target="_blank" class="thumbnail" href="${liveUrl}">
-          <img class="thumbnail__image" src="assets/${data.name}.jpg">
+          <img class="thumbnail__image" src="assets/thumbnails/${data.name}.jpg">
         </a>
         <div class="text-container">
           <div>
-            <a target="_blank" href="${liveUrl}">
-              <h3>${data.presentationName}</h3>
-            </a>
+            <h3>
+              <a target="_blank" href="${liveUrl}">
+                ${data.presentationName}
+              </a>
+            </h3>
             <p class="technologies">${data.technologies}</p>
             <p class="desc">${data.description}</p>
           </div>
-          <div>
-            <a target="_blank" href="${repoUrl}">Source Code</a> |
-            <a target="_blank" href="${liveUrl}">See Live</a>
+          <div class="buttons-container">
+            <a class="button" target="_blank" href="${repoUrl}">
+              Source Code
+              <img src="assets/icons/github.svg">
+            </a>
+            <a class="button" target="_blank" href="${liveUrl}">
+              See Live
+              <img src="assets/icons/webpage.svg">
+            </a>
           </div>
         </div>
       </li>
