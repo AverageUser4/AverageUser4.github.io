c448b8e302aeee91ba73f96b646ab05bd0084984
kacper <kacpersloka@o2.pl>
2023-06-12 16:26:45 +0200

    contact page change

--- js/page-contact.js
+++ js/page-contact.js
@@ -3,9 +3,7 @@ export default function render(container) {
     <div style="text-align: center;">
       <h2 style="margin-bottom: 16px;">Contact</h2>
     
-      <p class="para" style="margin: 0 auto;">
-        Email: kacperkrzysztofsloka@gmail.com
-      </p>
+      <p class="para" style="margin: 0 auto;">kacperkrzysztofsloka@gmail.com</p>
     </div>
   `;
 }
