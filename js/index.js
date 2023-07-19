dd3499e693e91029bc216cc5afcb9d6de0353c2a
kacper <kacpersloka@o2.pl>
2023-07-20 00:40:25 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import projectsRender from './page-projects.js';
 import techRender from './page-tech.js';
-import aboutRender from './page-about.js';
+// import aboutRender from './page-about.js';
 import contactRender from './page-contact.js';
 
-const render = { projectsRender, techRender, aboutRender, contactRender };
+const render = { projectsRender, techRender, contactRender, /* aboutRender */ };
 const main = document.querySelector('.main');
 const navList = document.querySelector('.nav-list');
 const navButtons = document.querySelectorAll('.nav-button');
@@ -14,7 +14,7 @@ navList.addEventListener('click', (event) => {
   switch(target.id) {
     case 'projects':
     case 'tech':
-    case 'about':
+    // case 'about':
     case 'contact':
       render[`${target.id}Render`](main);
       navButtons.forEach(button => button.classList.remove('nav-button--active'));
--- js/page-about.js
+++ js/page-about.js
@@ -4,12 +4,7 @@ export default function render(container) {
       <h2 style="margin-bottom: 16px;">About me</h2>
     
       <p class="para" style="margin: 0 auto;">
-        Hi, I'm Kacper. I'm self-taught programmer with almost 2 years of experience. 
-        So far I've been mostly building frontend of web applications (primarily using React),
-        but I also have basic experience with server-side scripting. I'm eager to learn and
-        my ultimate goal is to become a professional fullstack developer (although currently
-        I'm much more well-suited for performing frontend tasks). I'm looking for a job that
-        will let me grow as web developer, while bringing substantial value to company that hires me.
+        placeholder
       </p>
     </div>
   `;
